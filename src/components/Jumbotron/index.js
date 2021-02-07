807b73fc8e661b48cef2fe4785388ddb1e07b077
Kelly Smith <kdalton77@hotmail.com>
2021-02-06 19:44:53 -0500

    spelling error in jumbotron text

--- src/components/Jumbotron/index.js
+++ src/components/Jumbotron/index.js
@@ -5,7 +5,7 @@ function Jumbotron() {
     <div class="jumbotron jumbotron-fluid">
       <div class="container">
         <h1 class="display-4">Employee Directory</h1>
-        <p class="lead">Click on carrots to filter by heading or use the search box ot narrow your results.</p>
+        <p class="lead">Click on carrots to filter by heading or use the search box to narrow your results.</p>
       </div>
     </div>
   )
